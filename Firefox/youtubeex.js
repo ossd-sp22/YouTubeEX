c16aa0b1ddc94762ee78386e5a553531f91f6ce2
camilohv <72526400+camilohv@users.noreply.github.com>
2022-02-20 16:11:18 -0500

    moved Firefox files to their own directory

---
+++ Firefox/youtubeex.js
@@ -0,0 +1,22 @@
+// Hides side pannel of suggested videos
+document.getElementById("related").style.visibility = "hidden";
+
+// Hides comment section
+document.getElementById("comments").style.visibility = "hidden";
+
+// Hides homepage button
+document.getElementById("logo").style.visibility = "hidden";
+
+// Hides sidebar
+document.getElementById("scrim").style.visibility = "hidden";
+
+// Hides sidebar
+document.getElementById("guide").style.visibility = "hidden";
+
+// Hides suggested videos on homepage
+document.getElementById("contents").style.visibility = "hidden";
+
+// Hides endscreen suggested videos 
+document.getElementsByClassName('html5-endscreen')[0].style.visibility = 'hidden';
+
+
